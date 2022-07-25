3f8b304666b14b97ad511c2165aac1c7ad053106
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-25 18:00:01 -0500

    Add face class to the correct hiding place element

--- app.js
+++ app.js
@@ -45,8 +45,10 @@ function handleGuess(correctSpot, userGuess) {
     // then grab the appropriate container element for the correct guess from the DOM
     const correctHidingPlaceEl = document.getElementById(`${correctSpot}-container`);
     console.log(correctHidingPlaceEl);
-    
+
     // then add the face class to that element so that the face shows up
+    correctHidingPlaceEl.classList.add('face');
+    
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
 }
