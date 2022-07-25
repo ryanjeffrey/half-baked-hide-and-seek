eb426b695b47ef92d74ce606c37562ec6b652ba4
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-07-25 17:12:15 -0500

    Increment the total guesses

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ shedButton.addEventListener('click', () => {
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
     // then increment the guesses
+    totalGuesses++;
+    totalEl.textContent = totalGuesses;
+    
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
